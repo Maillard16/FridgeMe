4f7f31612117b7d3c5b4a075817871678ddad65d
anthony <roubaty>
2014-05-28 09:24:56 +0200

    Modif SqlConnection singleton & create DAO generic

--- Interface/src/dao/Dao.java
+++ Interface/src/dao/Dao.java
@@ -1,5 +1,44 @@
 package dao;
 
+import java.sql.Connection;
+
 public abstract class Dao<Element> {
+	protected Connection connect = null;
+
+	public Dao() {
+		connect = SqlConnexion.getConnection();
+	}
+
+	/**
+	 * Méthode de création
+	 * 
+	 * @param obj
+	 * @return boolean
+	 */
+	public abstract boolean create(Element obj);
+
+	/**
+	 * Méthode pour effacer
+	 * 
+	 * @param obj
+	 * @return boolean
+	 */
+	public abstract boolean delete(Element obj);
+
+	/**
+	 * Méthode de mise à jour
+	 * 
+	 * @param obj
+	 * @return boolean
+	 */
+	public abstract boolean update(Element obj);
+
+	/**
+	 * Méthode de recherche des informations
+	 * 
+	 * @param id
+	 * @return T
+	 */
+	public abstract Element find(int id);
 
 }
--- Interface/src/dao/SqlConnexion.java
+++ Interface/src/dao/SqlConnexion.java
@@ -3,20 +3,38 @@ package dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-public class SqlConnexion {
+import javax.management.InstanceAlreadyExistsException;
 
-	public Connection connect(){
-		Connection connect = null;
-		
-	    try {
-	      Class.forName("org.sqlite.JDBC");
-	      connect = DriverManager.getConnection("jdbc:sqlite:fridgeMe.db");
-	    } catch ( Exception e ) {
-	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	    }
-	    
-	    System.out.println("Connection to database: successfull");
-		
-		return connect;		
+public class SqlConnexion {
+	private static SqlConnexion instance = null;
+	private static Connection connect;
+
+	private SqlConnexion() {
+		try {
+			Class.forName("org.sqlite.JDBC");
+			connect = DriverManager.getConnection("jdbc:sqlite:fridgeMe.db");
+		} catch (Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+		}
+	}
+
+	/*
+	 * private Connection connect(){ Connection connect = null;
+	 * 
+	 * try { Class.forName("org.sqlite.JDBC"); connect =
+	 * DriverManager.getConnection("jdbc:sqlite:fridgeMe.db"); } catch (
+	 * Exception e ) { System.err.println( e.getClass().getName() + ": " +
+	 * e.getMessage() ); }
+	 * 
+	 * System.out.println("Connection to database: successfull");
+	 * 
+	 * return connect; }
+	 */
+
+	public static Connection getConnection() {
+		if (instance == null)
+			instance = new SqlConnexion();
+
+		return connect;
 	}
 }
